575cd74b8561b4dd59e686b16b84c1e89d7204db
rashmika <rashmikanethsarani119@gmail.com>
2025-05-23 23:44:54 +0530

    created add screen not completed

--- src/Screens/TaskScreen.screen.tsx
+++ src/Screens/TaskScreen.screen.tsx
@@ -1,4 +1,18 @@
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+
 export default function TaskScreen(){
     return(<>
+    <SafeAreaView>
+        <View >
+<Text>Daily Tasks</Text>
+        </View>
+     
+    </SafeAreaView>
+    
+
     </>)
 }
+const styles=StyleSheet.create({
+    
+})
